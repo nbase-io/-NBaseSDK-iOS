f7adbdcba31377ed7e4f783f8d7b5c509d08fba0
ShinBae Kong <osoriz@naver.com>
2024-01-08 22:16:17 -0800

    NBase framework

---
+++ NBase.framework/Headers/NBase.h
@@ -0,0 +1,18 @@
+//
+//  NBase.h
+//  NBase
+//
+//  Created by Kong Shinbae on 12/28/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NBase.
+FOUNDATION_EXPORT double NBaseVersionNumber;
+
+//! Project version string for NBase.
+FOUNDATION_EXPORT const unsigned char NBaseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NBase/PublicHeader.h>
+
+
